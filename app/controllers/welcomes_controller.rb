5d47cf07ea808adc0a79f18b8deed22cbf846d4e
JitenSabharwal <jitensabharwal08@gmail.com>
2015-11-10 14:49:37 +0530

    Modal Window

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,4 +1,21 @@
 class WelcomesController < ApplicationController
   def index
+ 	@user=User.new
   end
+  def new
+  	@user=User.new
+  end
+  def create
+  	@user=User.new(user_param)
+  	if @user.save
+  		flash[:note]="Registered Successfully"
+  	else
+  		flash[:note]="Registration Unsuccessful"
+  		redirect_to(:controller=>"welcomes",:action=>"new")
+  	end	
+  end
+  private
+  	def user_param
+  		params.require(:user).permit(:name,:email,:gender,:dob,:contact,:password)
+  	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   get 'tests/index'
 
   root 'welcomes#index'
-
+  resources :welcomes
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
